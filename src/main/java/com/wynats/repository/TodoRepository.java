a479424d125170c3eca5133f0a1e3b1d6037e2d0
Javi Cancela <cancelajavi@gmail.com>
2018-12-23 17:54:04 +0100

    added crossorigin to todorepository

--- src/main/java/com/wynats/repository/TodoRepository.java
+++ src/main/java/com/wynats/repository/TodoRepository.java
@@ -1,11 +1,11 @@
 package com.wynats.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.wynats.entity.Todo;
 
-
-public interface TodoRepository extends PagingAndSortingRepository<Todo,String> {
-	
+@CrossOrigin(origins = "http://localhost:4200")
+public interface TodoRepository extends PagingAndSortingRepository<Todo, String> {
 
 }
